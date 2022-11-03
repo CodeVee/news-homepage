589a9245c8a6eb1bb225fe2e2b800666f9690bd3
Victor Onyebuchi <victor.onyebuchi@ollasystems.com>
2022-11-03 12:35:35 +0100

    completed news box

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <script setup lang="ts">
 import BannerGrid from "./components/BannerGrid.vue";
 import NavBar from "./components/NavBar.vue";
+import NewsBox from "./components/NewsBox.vue";
 </script>
 
 <template>
@@ -11,6 +12,19 @@ import NavBar from "./components/NavBar.vue";
 
     <main>
       <BannerGrid />
+      <div class="grid grid-cols-3 gap-12 mb-48">
+        <NewsBox v-for="news in newsFeed" :key="news.id" :img="news.img">
+          <template #id>
+            {{news.id}}
+          </template>
+          <template #header>
+            {{news.header}}
+          </template>
+          <template #sub>
+            {{news.sub}}
+          </template>
+        </NewsBox>
+      </div>
     </main>
   </div>
 </template>
@@ -19,6 +33,26 @@ import NavBar from "./components/NavBar.vue";
 
 <script lang="ts">
 export default {
-  components: { NavBar, BannerGrid },
+  components: { NavBar, BannerGrid, NewsBox },
 };
+const newsFeed = [
+  { 
+    id: 1,
+    img: 'src/assets/images/image-retro-pcs.jpg',
+    header: 'Reviving Retro PCs',
+    sub: 'What happenss when old PCs are given modern upgrades?'
+  },
+  { 
+    id: 2,
+    img: 'src/assets/images/image-top-laptops.jpg',
+    header: 'Top 10 Laptops of 2022',
+    sub: 'Our best picks for various needs and budgets.'
+  },
+  { 
+    id: 3,
+    img: 'src/assets/images/image-gaming-growth.jpg',
+    header: 'The Growth of Gaming',
+    sub: 'How the pandemic has sparked fresh opportunities.'
+  }
+]
 </script>
---
+++ src/components/NewsBox.vue
@@ -0,0 +1,30 @@
+<template>
+  <div class="flex gap-[2.4rem]">
+    <img class=" w-40 h-[12.7rem]" :src="img" alt="retro" />
+    <div>
+      <h5 class="text-[3.2rem] leading-[3.2rem] font-bold text-blue-grayish">
+        0<slot name="id"></slot>
+    </h5>
+      <h6 class="text-[1.8rem] leading-[2.4rem] font-extrabold text-blue-vdark mt-[1.2rem] mb-[0.9rem]">
+        <slot name="header"></slot>
+     </h6>
+      <p class="text-2xl text-blue-grayishdark font-normal">
+        <slot name="sub"></slot>
+      </p>
+    </div>
+  </div>
+</template>
+
+<script lang="ts">
+export default {
+  name: "NewsBox",
+  props: {
+    img: String
+  },
+  mounted() {
+    console.log(this.img + 'sss');
+  }
+};
+</script>
+
+<style lang="scss" scoped></style>
